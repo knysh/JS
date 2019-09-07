31359f7d57e7b9628201521f6907c0195191dfa3
Yana Sitdikova <y.sitdikova@a1qa.com>
2019-09-07 21:30:18 +0300

    added logger

---
+++ test/helloworld.spec.js
@@ -0,0 +1,8 @@
+const { describe, it } = require('mocha');
+const logger = require('../utils/log.util')
+
+describe('Hello world TestSuite', () => {
+    it('should write "Hello world"', () => {
+        logger.info('Hello world');
+    });
+});
