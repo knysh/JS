42f60ca4baf63ad4cbff95b1b64ca7e932248ed8
Yana Sitdikova <y.sitdikova@a1qa.com>
2019-09-07 23:13:52 +0300

    added datetime tests

--- test/helloworld.spec.js
+++ test/helloworld.spec.js
@@ -1,5 +1,5 @@
 const { describe, it } = require('mocha');
-const logger = require('../utils/log.util')
+const logger = require('../utils/log.util');
 
 describe('Hello world TestSuite', () => {
     it('should write "Hello world"', () => {
