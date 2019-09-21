7eb3b1500aa37fcf34ce25a468a17976c30f3a7b
knysh <se.knysh@gmail.com>
2019-09-21 22:03:04 +0300

    added task 2

--- utils/wait.util.js
+++ utils/wait.util.js
@@ -1,31 +1,39 @@
 const logger = require("./log.util");
 
-const doWait = (action, interval) => {
+const doWait = (action, interval, expectedValue) => {
     return new Promise((resolve, reject) => {
-        if(action()){
+        if(action() == expectedValue){
             setTimeout(() => resolve(), interval);
         }
+        logger.warning(`Reject action value [${action()}]`)
         setTimeout(() => reject(), interval);
+    });
+}
+
+const waitFor = (action, maxCount, interval, count, waitValue) => {
+    count++;
+    logger.info(`[${count}] Wait for ${waitValue}`);
+    return doWait(action, interval, waitValue).then(() => {
+        logger.warning('Was able to reach expected condition!');
+        return true;
+    }, () => {
+        if(maxCount <= count){
+            logger.warning(`Was not able to reach expected condition! Action value is [${action()}]`);
+            return false;
+        }else{
+            return waitFor(action, maxCount, interval, count, waitValue);
+        }
     })
 }
 
-
-
 class Wait{
-    forTrue(action, maxCount, interval, count = 0){
-        count++;
-        logger.info(`[${count}] Wait for true`);
-        return doWait(action, interval).then(() => {
-            logger.warning('Was able to reach expected condition!');
-            return true;
-        }, () => {
-            if(maxCount <= count){
-                logger.warning('Was not able to reach expected condition!');
-                return false;
-            }else{
-                this.forTrue(action, maxCount, interval, count);
-            }
-        })
+    
+    forTrue(action, maxCount, interval){
+        return waitFor(action, maxCount, interval, 0, true);
+    }
+
+    forFalse(action, maxCount, interval){
+        return waitFor(action, maxCount, interval, 0, false);
     }
 }
 
